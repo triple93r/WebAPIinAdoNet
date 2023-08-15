c5d8bb2be10bbb10a78e96b158ed4f80623cceca
triple93r <56282248+triple93r@users.noreply.github.com>
2023-08-15 20:01:29 +0530

    initial

---
+++ YoutubeStudent/Controllers/StudentsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using YoutubeStudent.Models;
+
+namespace YoutubeStudent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private IConfiguration Configuration;
+        public StudentsController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        SqlConnection connString;
+        SqlCommand cmd;
+
+        [HttpGet]
+        public JsonResult Index()
+        {
+            List<Student> studlist = new List<Student>();
+            connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
+            DataTable dt = new DataTable();
+            cmd = new SqlCommand("select * from Students", connString);
+            connString.Open();
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            ad.Fill(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Student obj = new Student();
+                obj.Id = Convert.ToInt32(dt.Rows[i]["id"]);
+                obj.StudentName = dt.Rows[i]["StudentName"].ToString();
+                obj.age = Convert.ToInt32(dt.Rows[i]["Age"]);
+                obj.Address = dt.Rows[i]["Address"].ToString();
+                studlist.Add(obj);
+            }
+            connString.Close();
+            return Json(dt);
+        }
+
+    }
+}
---
+++ YoutubeStudent/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YoutubeStudent.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string StudentName { get; set; }
+        public int age { get; set; }
+        public string Address { get; set; } = String.Empty;
+    }
+}
