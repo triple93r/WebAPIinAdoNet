fc35e378325b04d2a5249f3c57e6f75f4eca0e61
triple93r <56282248+triple93r@users.noreply.github.com>
2023-08-18 06:07:26 +0530

    Update 4
    Multiple columns find

--- YoutubeStudent/Controllers/StudentsController.cs
+++ YoutubeStudent/Controllers/StudentsController.cs
@@ -16,53 +16,44 @@ namespace YoutubeStudent.Controllers
         }
         SqlConnection connString;
         SqlCommand cmd;
+        SqlDataAdapter adap;
+        DataTable dtb;
 
         [HttpGet]
         public JsonResult Index() //GET
         {
-            List<Student> studlist = new List<Student>();
             connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
             DataTable dt = new DataTable();
             cmd = new SqlCommand("select * from Students", connString);
-            connString.Open();
-            SqlDataAdapter ad = new SqlDataAdapter(cmd);
-            ad.Fill(dt);
-            for (int i = 0; i < dt.Rows.Count; i++)
+            try
             {
-                Student obj = new Student();
-                obj.Id = Convert.ToInt32(dt.Rows[i]["id"]);
-                obj.StudentName = dt.Rows[i]["StudentName"].ToString();
-                obj.age = Convert.ToInt32(dt.Rows[i]["Age"]);
-                obj.Address = dt.Rows[i]["Address"].ToString();
-                studlist.Add(obj);
+                connString.Open();
+                SqlDataAdapter ad = new SqlDataAdapter(cmd);
+                ad.Fill(dt);
+                return Json(dt);
             }
-            connString.Close();
-            return Json(dt);
+            catch (Exception ef)
+            {
+                return Json(StatusCodes.Status500InternalServerError, ef.Message);
+            }
+            finally { connString.Close(); }
         }
 
         [Route("FindById/{id}")] //GET
         [HttpGet]
         public JsonResult FindById(int id)
         {
-            List<Student> studlist = new List<Student>();
             connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
-            DataTable dt = new DataTable();
+            dtb = new DataTable();
             cmd = new SqlCommand("select * from Students where id="+id, connString);
             try
             {
                 connString.Open();
-                SqlDataAdapter ad = new SqlDataAdapter(cmd);
-                ad.Fill(dt);
-                if(dt.Rows.Count>0)
+                adap = new SqlDataAdapter(cmd);
+                adap.Fill(dtb);
+                if(dtb.Rows.Count>0)
                 {
-                    Student obj = new Student();
-                    obj.Id = Convert.ToInt32(dt.Rows[0]["id"]);
-                    obj.StudentName = dt.Rows[0]["StudentName"].ToString();
-                    obj.age = Convert.ToInt32(dt.Rows[0]["Age"]);
-                    obj.Address = dt.Rows[0]["Address"].ToString();
-                    studlist.Add(obj);
-                    connString.Close();
-                    return Json(dt);
+                    return Json(dtb);
                 }
                 else
                     return Json("Not Found!");
@@ -71,7 +62,89 @@ namespace YoutubeStudent.Controllers
             {
                 return Json(StatusCodes.Status500InternalServerError, ef.Message);
             }
+            finally { connString.Close(); }
         }
+
+
+        [Route("FindByParams/{col}/{val}")] //GET
+        [HttpGet]
+        public JsonResult FindByParams(string col, string val)
+        {
+            connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
+            dtb = new DataTable();
+            cmd = new SqlCommand("select * from Students where "+col+"='"+val+"'", connString);
+            try
+            {
+                connString.Open();
+                adap = new SqlDataAdapter(cmd);
+                adap.Fill(dtb);
+                if (dtb.Rows.Count > 0)
+                {
+                   return Json(dtb);
+                }
+                else
+                   return Json("Not Found!");
+            }
+            catch (Exception ef)
+            {
+                return Json(StatusCodes.Status500InternalServerError, ef.Message);
+            }
+            finally { connString.Close(); }
+        }
+
+
+        [Route("FindByColumnVal/{val}")] //GET
+        [HttpGet]
+        public JsonResult FindByColumnVal(string val)
+        {
+            connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
+            dtb = new DataTable();
+            cmd = new SqlCommand("select * from Students where Address='"+ val +"'", connString);
+            try
+            {
+                connString.Open();
+                adap = new SqlDataAdapter(cmd);
+                adap.Fill(dtb);
+                if (dtb.Rows.Count > 0)
+                {
+                    return Json(dtb);
+                }
+                else
+                    return Json("Not Found!");
+            }
+            catch (Exception ef)
+            {
+                return Json(StatusCodes.Status500InternalServerError, ef.Message);
+            }
+            finally { connString.Close(); }
+        }
+
+        [Route("FindByMultiColumnVal/{val1}/{val2}")] //GET
+        [HttpGet]
+        public JsonResult FindByMultiColumnVal(string val1, int val2)
+        {
+            connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
+            dtb = new DataTable();
+            cmd = new SqlCommand("select * from Students where Address='" + val1 + "' and age>"+val2, connString);
+            try
+            {
+                connString.Open();
+                adap = new SqlDataAdapter(cmd);
+                adap.Fill(dtb);
+                if (dtb.Rows.Count > 0)
+                {
+                    return Json(dtb);
+                }
+                else
+                    return Json("Not Found!");
+            }
+            catch (Exception ef)
+            {
+                return Json(StatusCodes.Status500InternalServerError, ef.Message);
+            }
+            finally { connString.Close(); }
+        }
+
 
         [Route("Create")] //POST
         [HttpPost]
@@ -92,28 +165,28 @@ namespace YoutubeStudent.Controllers
             }
         }
 
-        [Route("Edit")]
-        [HttpPost] //PUT
-        public ActionResult Edit(Student st)
-        {
-            try
-            {
-                connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
-                cmd = new SqlCommand("update students set StudentName='" + st.StudentName + "', age= " + st.age + ", [Address]='" + st.Address + "' where Id=" + st.Id + "", connString);
-                connString.Open();
-                int x = cmd.ExecuteNonQuery();
-                if (x > 0)
-                {
-                    return Ok(new { Message = "Record Updated" });
-                }
-                return BadRequest(new { Message = "Record Not found!" });
-            }
-            catch (Exception ef)
-            {
-                return BadRequest(ef.Message);
-            }
-            finally { connString.Close(); }
-        }
+        //[Route("Edit")]
+        //[HttpPost] //PUT
+        //public ActionResult Edit(Student st)
+        //{
+        //    try
+        //    {
+        //        connString = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
+        //        cmd = new SqlCommand("update students set StudentName='" + st.StudentName + "', age= " + st.age + ", [Address]='" + st.Address + "' where Id=" + st.Id + "", connString);
+        //        connString.Open();
+        //        int x = cmd.ExecuteNonQuery();
+        //        if (x > 0)
+        //        {
+        //            return Ok(new { Message = "Record Updated" });
+        //        }
+        //        return BadRequest(new { Message = "Record Not found!" });
+        //    }
+        //    catch (Exception ef)
+        //    {
+        //        return BadRequest(ef.Message);
+        //    }
+        //    finally { connString.Close(); }
+        //}
 
     }
 }
